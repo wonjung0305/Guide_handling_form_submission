a1c34718abe0e1978fd4216839b9fc3bb9903d49
wonjung <wonjung1919@gmail.com>
2026-07-23 10:37:03 +0900

    chore: 프로젝트 초기 생성

---
+++ src/main/java/com/example/gshandlingformsubmission/GsHandlingFormSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.gshandlingformsubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsHandlingFormSubmissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsHandlingFormSubmissionApplication.class, args);
+    }
+
+}
